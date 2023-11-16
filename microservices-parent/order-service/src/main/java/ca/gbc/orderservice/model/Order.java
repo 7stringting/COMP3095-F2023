5479fc9c45ce0a1818f9dcb65da9433aa732cd2f
ali <a.aloreabi2000@gmail.com>
2023-11-15 23:22:02 -0500

    order-services added

---
+++ microservices-parent/order-service/src/main/java/ca/gbc/orderservice/model/Order.java
@@ -0,0 +1,29 @@
+package ca.gbc.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+import java.util.List;
+
+@Entity
+@Table(name = "t_order")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String OrderNumber;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade =CascadeType.ALL)
+    private List<OrderLineItem> orderLineItemList;
+
+
+}
