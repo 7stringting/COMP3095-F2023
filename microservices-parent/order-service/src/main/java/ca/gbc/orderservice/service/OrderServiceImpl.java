8ac2a1438bed20420430406c4edafa85c067e7e3
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2023-11-18 21:03:33 -0500

    application.properties added

--- microservices-parent/order-service/src/main/java/ca/gbc/orderservice/service/OrderServiceImpl.java
+++ microservices-parent/order-service/src/main/java/ca/gbc/orderservice/service/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package ca.gbc.orderservice.service;
 
+import ca.gbc.orderservice.dto.InventoryRequest;
+import ca.gbc.orderservice.dto.InventoryResponse;
 import ca.gbc.orderservice.dto.OrderLineItemDto;
 import ca.gbc.orderservice.dto.OrderRequest;
 import ca.gbc.orderservice.model.Order;
@@ -7,8 +9,10 @@ import ca.gbc.orderservice.model.OrderLineItem;
 import ca.gbc.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,22 +21,57 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Transactional
 public class OrderServiceImpl implements OrderService{
+    private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public final OrderRepository orderRepository;
-
+    @Value("${inventory.service.url}")
+    private String inventoryApiUri;
     @Override
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        List<OrderLineItem> orderLineItems =
-                orderRequest.getOrderLineItemDtoList().stream().map(this::mapToDto).toList();
-
+        List<OrderLineItem> orderLineItems=orderRequest
+                .getOrderLineItemDtoList()
+                .stream()
+                .map(this::mapToDo)
+                .toList();
         order.setOrderLineItemList(orderLineItems);
+
+        List<InventoryRequest> inventoryRequests = order.getOrderLineItemList()
+                .stream().map(orderLineItem -> InventoryRequest.builder()
+                        .skuCode(orderLineItem.getSkuCode())
+                        .quantity(orderLineItem.getQuantity())
+                        .build())
+                .toList();
+
+        List<InventoryResponse> inventoryResponsesList = webClient
+                .post()
+                .uri(inventoryApiUri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(inventoryRequests)
+                .retrieve()
+                .bodyToFlux(InventoryResponse.class).collectList()
+                .block();
+
+        assert inventoryResponsesList != null;
+        boolean allProductsInStock = inventoryResponsesList
+                .stream()
+                .allMatch(InventoryResponse::isSufficientStock);
+        if(Boolean.TRUE.equals(allProductsInStock)){
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("not all products are in stock");
+        }
+
+
+
+
         orderRepository.save(order);
+
     }
 
-    private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDto){
+    private OrderLineItem mapToDo(OrderLineItemDto orderLineItemDto){
         OrderLineItem orderLineItem = new OrderLineItem();
         orderLineItem.setPrice(orderLineItemDto.getPrice());
         orderLineItem.setQuantity(orderLineItemDto.getQuantity());
